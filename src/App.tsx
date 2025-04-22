5907697b3d0e2c9beb87ed08ac60f60a3cf55889
Ian Chiu <nk9710632@gmail.com>
2025-04-22 21:30:05 +0800

    feat: change root background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Box } from '@mui/material';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import TodoList from './pages/TodoList';
@@ -15,30 +16,42 @@ const theme = createTheme({
       main: '#dc004e',
     },
   },
-})
+});
 
 function App() {
   // TODO: Implement authentication
-  const isAuthenticated = false;
+  const isAuthenticated = true;
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/todo"
-            element={
-              isAuthenticated ? <TodoList /> : <Navigate to="/login" replace />
-            }
-          />
-          <Route path="/" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </BrowserRouter>
+      <Box
+        sx={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: '#dcdddf',
+          overflow: 'auto',
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/todos"
+              element={
+                isAuthenticated ? <TodoList /> : <Navigate to="/login" replace />
+              }
+            />
+            <Route path="/" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </Box>
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
